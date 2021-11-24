810e37958f5950dd4a887e2ccfbd52a41f793b5d
dv.rakry <dv.rakry@gmail.com>
2021-11-24 15:50:59 +0900

    11.24-3

---
+++ src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h5> {{roomInfo[0].title}} </h5>
+      <p> {{roomInfo[0].content}} </p>
+      <p>{{roomInfo[0].date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'list',
+    props : {
+        roomInfo : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
