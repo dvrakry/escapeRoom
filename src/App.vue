0ac01b43d250f96b43e801b247bef406fe6b1ec5
dv.rakry <dv.rakry@gmail.com>
2021-11-24 15:41:32 +0900

    11.24

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Vuelog</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <router-link to="/list">LIST</router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<router-view :roomInfo="roomInfo"></router-view>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import roomInfo from './assets/blog.js';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      roomInfo : roomInfo,
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+import routerMake from './router';
+
+createApp(App).use(routerMake).mount('#app')
